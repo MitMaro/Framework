2b112a2ca797c93fa49478eac18262b04d8fe5ff
Tim Oram <mitmaro@mitmaro.ca>
2011-01-13 03:20:35 -0330

    Fixed doc block on AccessControl.php

--- lib/Framework/AccessControl.php
+++ lib/Framework/AccessControl.php
@@ -1,12 +1,10 @@
 <?php
 /**
- * @category  AccessControl
  * @package  Framework
- * @subpackage AccessControl
  * @version  0.1.0
- * @author  Tim Oram <mitmaro@mitmaro.ca>
- * @copyright  Copyright (c) 2010 Tim Oram (http://www.mitmaro.ca)
- * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2010 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
  */
 
 namespace Framework;
