0f02d30f8bb5804a14c673380dd58515786374b9
Tim Oram <mitmaro@mitmaro.ca>
2011-01-14 06:04:13 -0330

    More detailed documentation on the autoloader

--- lib/Framework/Autoloader.php
+++ lib/Framework/Autoloader.php
@@ -1,12 +1,69 @@
 <?php
 /**
- * @category  Autoloader
- * @package  Framework
- * @subpackage Autoloader
+ * A simple yet powerful namespace based autoloader for php.
+ *
+ *
+ * <h4>Basic Usage</h4>
+ * The most basic usage of the autoloader is to map a namespace to a directory like so:
+ * 
+ * <pre>
+ *     $autoloader = new \Framework\Autoloader();
+ *     $autoloader->addNamespace('MyNameSpace', '/path/to/namespace/root/')
+ * </pre>
+ *
+ * This will autoload all classes that begin with the namespace MyNameSpace to that directory using
+ * the remainder of the namespace to figure out the path. That is, using the autloader setup from
+ * above, the class '\MyNameSpace\Admin\User' would map to '/path/to/namespace/root/Admin/User.php'.
+ *
+ * <h4>Mutltiple Directories Per Namespace</h4>
+ * It is possible to add mulitple directories to a namespace by calling the addNamespace method
+ * on the same name space more than once. For example:
+ * 
+ * <pre>
+ *     $autoloader = new \Framework\Autoloader();
+ *     $autoloader->addNamespace('MyNameSpace', '/path/to/namespace/root/')
+ *     $autoloader->addNamespace('MyNameSpace', '/second/path/to/namespace/root/')
+ * </pre>
+ *
+ * Using this setup the autoloader will first look into the first directory provided and if it is
+ * unable to find the file it will move on to the next directory.
+ *
+ *
+ * <h4>Advanced Usage: Patterns</h4>
+ * Another way to use the autoloader is by using the simple pattern matching system. You provide
+ * tokens in the path when you register the namespace that will be replaced using values from the
+ * class namespace. Here is an example that explains the power of this system.
+ *
+ * Assume you have a directory structure like this:
+ * 
+ * <pre>
+ *   | MyProject
+ *   |    -> modules
+ *   |       -> Admin
+ *   |           -> controllers
+ *   |                 -> Home.php           (class: \MyProject\Admin\Home)
+ *   |                 -> AccessControl.php  (class: \MyProject\Admin\AccessControl)
+ * </pre>
+ *
+ * Lets assume you want to autoload the controllers in the modules folder. We know that the basic
+ * autoloader would not work because the controllers folder is not in the namespace, that is the
+ * namespace is not like '\MyProject\Admin\controllers\Home'. To overcome this problem you can write
+ * a pattern to auto add the controllers directory dynamically.
+ * 
+ * <pre>
+ *     $autoloader = new \Framework\Autoloader();
+ *     $autoloader->addNamespace('MyProject', '/path/to/MyProject/modules/:1/controllers/')
+ * </pre>
+ *
+ * If you now tried to autoload the \MyProject\Admin\AccessControl is would now map to the directory
+ * '/path/to/MyProject/modules/Admin/controllers/AccessControl.php' instead of
+ * '/path/to/MyProject/modules/Admin/AccessControl.php'.
+ * 
+ * @package  Framework\AccessControl
  * @version  0.1.0
- * @author  Tim Oram <mitmaro@mitmaro.ca>
- * @copyright  Copyright (c) 2010 Tim Oram (http://www.mitmaro.ca)
- * @license    http://www.opensource.org/licenses/mit-license.php  The MIT License
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2010 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
  */
 
 namespace Framework;
