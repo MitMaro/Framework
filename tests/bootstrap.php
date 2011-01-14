cf4a3f7967daa4f3f11d83fa4314c631e4381cd5
Tim Oram <mitmaro@mitmaro.ca>
2011-01-14 01:47:11 -0330

    Added initial PHPUnit testing architecture

---
+++ tests/bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @package  Framework\Test
+ * @version  0.1.0
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2010 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
+ */
+
+require 'config.php';
+
+// add site's server path (root) to include path
+set_include_path(
+	realpath($cfg['project_root']) . PATH_SEPARATOR .
+	$cfg['pear_path']
+);
+
---
+++ tests/config.dist.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @package  Framework\Test
+ * @version  0.1.0
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2010 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
+ */
+/*
+ Instructions:
+  Make a copy of this file and call it config.php, edit and enjoy
+*/
+
+$cfg = array();
+
+// path info
+$cfg['project_root'] = '';
+$cfg['pear_path'] = '';
