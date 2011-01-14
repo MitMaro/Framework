26630a3d3573ec5683f913007ba0c2458b696ce5
Tim Oram <mitmaro@mitmaro.ca>
2011-01-14 02:02:11 -0330

    Added Request module with tests

---
+++ lib/Framework/Request.php
@@ -0,0 +1,244 @@
+<?php
+/**
+ * @package  Framework
+ * @version  0.1.0
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2010 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
+ * @dependencies
+ * <ul>
+ * <li>\Framework\Action</li>
+ * <li>\Framework\Session (Optional)</li>
+ * </ul>
+ */
+
+namespace Framework;
+
+class Request {
+
+	/**
+	 * An associative array of parameters
+	 * @var array
+	 */
+	protected $params = array();
+	
+	/**
+	 * An associative array of url mappers in the format (pattern => callback)
+	 */
+	protected $url_maps = array();
+	
+	/**
+	 * Handle a request
+	 *
+	 * @param string $url (Optional) The request URL, set to override the auto dectection
+	 */
+	public function beginRequest($url = null) {
+		$callback = $this->resolveUrl($url);
+		$this->setParams();
+		
+		if (class_exists('\Framework\Session')) {
+			$this->setReferrer($url);
+		}
+		
+		$class_name = $callback['handler']['class'];
+		$method_name = $callback['handler']['method'];
+		
+		// create the controller, and run
+		$action = new $class_name($this);
+		$action->_setup();
+		$action->_preInit();
+		$action->_init();
+		$action->_postInit();
+		// verify user access
+		if (!$action->_checkAccess()) {
+			return $action->_accessDenied();
+		}
+		$action->_preAction();
+		call_user_func_array(array($action, $method_name), $callback['params']);
+		$action->_postAction();
+		$action->_preRender();
+		$action->_render();
+		$action->_postRender();
+	}
+	
+	/**
+	 * Adds patterns to the url pattern mapper
+	 *
+	 * @param array $patterns An array of key value pairs in the form (pattern => class.method)
+	 */
+	public function addUrlPatterns($patterns) {
+		
+		// loop over the array of key value pairs, validating and adding to the url map
+		foreach ($patterns as $pattern => $handler) {
+			
+			// valid pattern
+			if (!is_string($pattern)) {
+				throw new Request\Exception('The pattern provided is invalid, must be a valid regular expression');
+			}
+			
+			$parts = explode('.', $handler);
+			
+			// valid handler
+			if (count($parts) != 2) {
+				throw new Request\Exception('The handler provided is invalid, must be a valid php callback');
+			}
+			$this->url_maps[$pattern] = array('class' => $parts[0], 'method' => $parts[1]);
+		}
+	}
+	
+	/**
+	 * Get the url patterns
+	 *
+	 * @return array The url patterns array
+	 */
+	public function getUrlPatternMap() {
+		return $this->url_maps;
+	}
+	
+	/**
+	 * Resolve a url to a callable php function
+	 *
+	 * @param string $url An optional url string, if not provided will 
+	 */
+	public function resolveUrl($url = false) {
+		
+		// if no url was provided get it from the _SERVER super global
+		if (!$url) {
+			if (isset($_SERVER['PATH_INFO'])) {
+				$url = $_SERVER['PATH_INFO'];
+			}
+			else if (isset($_SERVER['REQUEST_URI'])) {
+				$url = $_SERVER['REQUEST_URI'];
+			}
+			else {
+				throw new Request\Exception('Was unable to retrive URL from _SERVER super global');
+			}
+		}
+		
+		// $params will contain any subpattern matches found in the regular expression
+		$params = array();
+		foreach ($this->url_maps as $pattern => $handler) {
+			if (preg_match($pattern, $url, $params)) {
+				unset($params[0]); // remove the global match
+				return array('handler' => $handler, 'params' => $params);
+			}
+		}
+		return false;
+		
+	}
+	
+	/**
+	 * Set the parameters from the request
+	 */
+	public function setParams() {
+		$this->params = array_merge($this->params, $_GET);
+		$this->params = array_merge($this->params, $_POST);
+		
+		// merge in pushed over request params when supplied
+		if (class_exists('\Framework\Session')) {
+			if (($params = \Framework\Session::getOnce('_params', false, '___FRAMEWORK___')) != false) {
+				$this->params = array_merge($this->params, $params);
+			}
+		}
+		
+	}
+	
+	/**
+	 * Get the parameters of the request
+	 *
+	 * @return array An array of parameter key-value pairs
+	 */
+	public function getParams() {
+		return $this->params;
+	}
+	
+	/**
+	 * Get a single parameter from the request
+	 *
+	 * @param string $name The key of the parameter
+	 * @param string $default (optional) A default value to return, defaults to null
+	 *
+	 * @return mixed The value associated with the key provided or the default value if key is not found
+	 */
+	public function getParam($name, $default = null) {
+		// check availablity and type
+		if (isset($this->params[$name])) {
+			return $this->params[$name];
+		}
+		return $default;
+	}
+	
+	/**
+	 * Add a parameter to the list of parameters
+	 *
+	 * @param string $name The name of the parameter
+	 * @param mixed $value The value of the parameter
+	 */
+	public function addParam($name, $value) {
+		$this->params[$name] = $value;
+	}
+	
+	/**
+	 * Check is parameter of $name exists
+	 *
+	 * @param string $name The parameter name
+	 *
+	 * @return boolean True is the parameter exists, otherwise false
+	 */
+	public function hasParam($name) {
+		return isset($this->params[$name]);
+	}
+	
+	/**
+	 * Get the referrer
+	 *
+	 * @param string $default (optional) The default referrer
+	 *
+	 * @return string The referrer or the default to if one could not be found and the default was provided, false otherwise.
+	 */
+	public function getReferrer($default = false) {
+		
+		// try the one stored by this application first
+		
+		// merge in pushed over request params when supplied
+		if (class_exists('\Framework\Session')) {
+			$referrer = Session::getOnce('referrer', false, '___FRAMEWORK___');
+			if ($referrer) {
+				return $referrer;
+			}
+		}
+		
+		if (isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'] != '') {
+			return $_SERVER['HTTP_REFERER'];
+		}
+		
+		return $default;
+	}
+	
+	/**
+	 * Sets the referrer
+	 *
+	 * @param string $url The url of the referrer
+	 */
+	public function setReferrer($url) {
+		// save referreral
+		if (!class_exists('\Framework\Session')) {
+			throw new Request\Exception('The Session module is required to use ' . __METHOD__);
+		}
+		Session::setOnce('referrer', $url, self::$namespace);
+		
+	}
+	
+	/**
+	 * Determine if the request was an ajax based request, this is supported
+	 * by all the major libaries, the correct header should be added if using
+	 * xmlHttpRequest directly.
+	 *
+	 * @return boolean True if the request is a AJAX based request, otherwise false
+	 */
+	public static function isAjax() {
+		return isset($_SERVER['HTTP_X_REQUESTED_WITH']) &&
+			strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest';
+	}
+	
+}
---
+++ tests/Framework/RequestTest.php
@@ -0,0 +1,267 @@
+<?php
+/**
+ * @package  Framework/Test
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2011 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
+ */
+
+require_once 'Framework/Request.php';
+
+use
+	\Framework\Request
+;
+
+class Request_Test extends PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @covers \Framework\Request::addUrlPatterns
+	 * @covers \Framework\Request::getUrlPatternMap
+	 */
+	public function testAddUrlPatterns_valid() {
+		$request = new Request();
+		$request->addUrlPatterns(array('abc' => 'def.ghi'));
+		
+		$this->assertEquals(
+			array('abc' => array('class' => 'def', 'method' => 'ghi')),
+			$request->getUrlPatternMap()
+		);
+		
+		$request->addUrlPatterns(array(
+			'def' => 'abc2.def',
+			'cde' => 'abc.def',
+			'abc' => 'abc.def'
+		));
+		
+		$this->assertEquals(
+			array(
+				'def' => array('class' => 'abc2', 'method' => 'def'),
+				'cde' => array('class' => 'abc', 'method' => 'def'),
+				'abc' => array('class' => 'abc', 'method' => 'def'),
+			),
+			$request->getUrlPatternMap()
+		);
+		
+	}
+	
+	/**
+	 * @covers \Framework\Request::addUrlPatterns
+	 * @expectedException \Framework\Request\Exception
+	 */
+	public function testAddUrlPatterns_invalidPattern() {
+		$request = new Request();
+		$request->addUrlPatterns(array(false => 'abc.def'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::addUrlPatterns
+	 * @expectedException \Framework\Request\Exception
+	 */
+	public function testAddUrlPatterns_invalidHandler() {
+		$request = new Request();
+		$request->addUrlPatterns(array('abc' => 'abc'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::resolveUrl
+	 * @expectedException \Framework\Request\Exception
+	 */
+	public function testResolveUrl_NoUrlNoAutoFind() {
+		$request = new Request();
+		$request->resolveUrl();
+	}
+	
+	/**
+	 * @covers \Framework\Request::resolveUrl
+	 */
+	public function testResolveUrl_TestGlobalsPATH_INFO() {
+		$request = new Request();
+		$request->addUrlPatterns(array('/abc/' => 'abc.def'));
+		$_SERVER['PATH_INFO'] = 'abc';
+		
+		$this->assertEquals(
+			array(
+				'handler' => array(
+					'class' => 'abc',
+					'method' => 'def'
+				),
+				'params' => array()
+			),
+			$request->resolveUrl()
+		);
+	}
+	
+	/**
+	 * @covers \Framework\Request::resolveUrl
+	 */
+	public function testResolveUrl_TestGlobalsREQUEST_URI() {
+		$request = new Request();
+		$request->addUrlPatterns(array('/def/' => 'abc.def'));
+		unset($_SERVER['PATH_INFO']); // to be sure
+		$_SERVER['REQUEST_URI'] = 'def';
+		
+		$this->assertEquals(
+			array(
+				'handler' => array(
+					'class' => 'abc',
+					'method' => 'def'
+				),
+				'params' => array()
+			),
+			$request->resolveUrl()
+		);
+	}
+	
+	/**
+	 * @covers \Framework\Request::resolveUrl
+	 */
+	public function testResolveUrl_NoMatch() {
+		$request = new Request();
+		$request->addUrlPatterns(array('/abc/' => 'abc.def'));
+		
+		$this->assertFalse($request->resolveUrl('def'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::resolveUrl
+	 */
+	public function testResolveUrl_Params() {
+		$request = new Request();
+		$request->addUrlPatterns(array('#abc/(?<id>[0-9]+)#' => 'abc.def'));
+		
+		
+		$this->assertEquals(
+			array(
+				'handler' => array(
+					'class' => 'abc',
+					'method' => 'def'
+				),
+				'params' => array('id' => '12', 1 => '12')
+			),
+			$request->resolveUrl('abc/12')
+		);
+	}
+	
+	/**
+	 * @covers \Framework\Request::setParams
+	 * @covers \Framework\Request::getParams
+	 */
+	public function testSetParams_GETOnly() {
+		$request = new Request();
+		$_GET = array(
+			'abc' => 'def'
+		);
+		$request->setParams();
+		$this->assertEquals(
+			array(
+				'abc' => 'def'
+			),
+			$request->getParams()
+		);
+	}
+	
+	/**
+	 * @covers \Framework\Request::addParam
+	 * @covers \Framework\Request::setParams
+	 * @covers \Framework\Request::getParams
+	 */
+	public function testSetParams_AddGETAndPOST() {
+		$request = new Request();
+		$_GET = array(
+			'a' => '2',
+			'c' => '3',
+			'd' => '4'
+		);
+		$_POST = array(
+			'd' => '5',
+			'e' => '6'
+		);
+		$request->addParam('a', '0');
+		$request->addParam('b', '1');
+		$request->setParams();
+		$this->assertEquals(
+			array(
+				'b' => '1',
+				'a' => '2',
+				'c' => '3',
+				'd' => '5',
+				'e' => '6'
+			),
+			$request->getParams()
+		);
+	}
+	
+	/**
+	 * @covers \Framework\Request::addParam
+	 * @covers \Framework\Request::getParam
+	 */
+	public function testSetParams_GetParamExists() {
+		$request = new Request();
+		
+		$request->addParam('a', '1');
+		$this->assertEquals('1', $request->getParam('a'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::addParam
+	 * @covers \Framework\Request::getParam
+	 */
+	public function testSetParams_GetParamNotExists() {
+		$request = new Request();
+		
+		$this->assertNull($request->getParam('a'));
+		$this->assertEquals('1', $request->getParam('a', '1'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::addParam
+	 * @covers \Framework\Request::hasParam
+	 */
+	public function testHasParams() {
+		$request = new Request();
+		
+		$request->addParam('a', '1');
+		
+		$this->assertTrue($request->hasParam('a'));
+		$this->assertFalse($request->hasParam('b'));
+	}
+	
+	/**
+	 * @covers \Framework\Request::getReferrer
+	 */
+	public function testGetReferrer_NotFound() {
+		$request = new Request();
+		
+		$this->assertFalse($request->getReferrer());
+		$this->assertEquals('abc', $request->getReferrer('abc'));
+		
+	}
+	
+	/**
+	 * @covers \Framework\Request::getReferrer
+	 */
+	public function testGetReferrer_Global() {
+		$request = new Request();
+		$_SERVER['HTTP_REFERER'] = 'def';
+		$this->assertEquals('def', $request->getReferrer());
+		
+	}
+	
+	/**
+	 * @covers \Framework\Request::isAjax
+	 */
+	public function testIsAjax_IsNot() {
+		$_SERVER['HTTP_X_REQUESTED_WITH'] = 'notajax';
+		$this->assertFalse(Request::isAjax());
+		
+	}
+	
+	/**
+	 * @covers \Framework\Request::isAjax
+	 */
+	public function testIsAjax_Is() {
+		$_SERVER['HTTP_X_REQUESTED_WITH'] = 'xmlhttprequest';
+		$this->assertTrue(Request::isAjax());
+	}
+	
+}
