8598c7a82d01875b9497573209a3c802bca21f23
Tim Oram <mitmaro@mitmaro.ca>
2011-04-03 02:39:48 -0230

    Added test for Error Handler logger exception

---
+++ tests/Framework/ErrorHandler/Exception/LoggerTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package  Framework/Test
+ * @author  Tim Oram (mitmaro@mitmaro.ca)
+ * @copyright  Copyright 2011 Tim Oram (<a href="http://www.mitmaro.ca">www.mitmaro.ca</a>)
+ * @license  <a href="http://www.opensource.org/licenses/mit-license.php">The MIT License</a>
+ */
+
+namespace Framework\Tests\View\Exception;
+
+use
+	\Framework\ErrorHandler\Exception\Logger
+;
+
+class Logger_Test extends \PHPUnit_Framework_TestCase {
+	
+	/**
+	 * @covers \Framework\ErrorHandler\Exception\Logger
+	 */
+	public function testAll() {
+		new Logger();
+	}
+}
